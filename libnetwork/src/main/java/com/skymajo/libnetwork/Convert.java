08abf1a0d14274d834ac12c659a1fc24c5cc3a2f
SkyMajo <BraveNixo@Gmail.com>
2020-04-29 18:35:22 +0800

    解决bug

--- libnetwork/src/main/java/com/skymajo/libnetwork/Convert.java
+++
@@ -1,8 +0,0 @@
-package com.skymajo.libnetwork;
-
-import java.lang.reflect.Type;
-
-public interface Convert<T> {
-    T convert(String reponse , Type type);
-    T convert(String reponse , Class clz);
-}
